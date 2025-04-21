8d40fc1e4fd36d49b72243c558a6776ca5018170
andrew <andrewkalumba29@gmail.com>
2025-04-21 21:09:46 +0200

    made a few css changes and added animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AnimalGroupPage from './pages/AnimalGroupPage'
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './pages/Layout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [wildLife, setWildLife] = useState(null)
@@ -11,6 +12,7 @@ function App() {
   }
 
   return (
+    <Router>
     <Routes>
       <Route element={<Layout updateFunction2={updateFunction} />}>
         <Route path="/" element={<AnimalGroupPage groupName="all" updateFunction2={updateFunction} wildLife2={wildLife} isHomePage={true} />} />
@@ -19,6 +21,7 @@ function App() {
         <Route path="/reptiles" element={<AnimalGroupPage groupName="reptiles" updateFunction2={updateFunction} wildLife2={wildLife} />} />
       </Route>
     </Routes>
+    </Router>
   )
 }
 
